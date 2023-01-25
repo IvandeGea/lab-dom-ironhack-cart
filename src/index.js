ddf34dd207be4244d2672e1073b95605b427bd33
Ivan <Ivandgea@gmail.com>
2023-01-25 18:28:47 +0100

    Lab solved

--- src/index.js
+++ src/index.js
@@ -3,22 +3,56 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+
+
+  const subtotal = price * quantity;
+
+  let subTotal = product.querySelector('.subtotal span');
+
+  subTotal.innerHTML = subtotal;
+
+
+  return subtotal
   //... your code goes here
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
+  let multipleProduct = document.getElementsByClassName(`product`);
+
+
+  let totalProduct = 0;
+
+  for (let i = 0; i < multipleProduct.length; i++) {
+    totalProduct += updateSubtotal(multipleProduct[i])
+
+  }
+  let total = document.querySelector('#total-value span');
+
+  total.innerHTML = totalProduct;
+
+
 }
+// Esto seria tranformandolo en una Array y poder aplicar el .forEach
+// const multipleProduct = Array.from(document.getElementsByClassName('product'));
+// multipleProduct.forEach( (product) => {
+//   updateSubtotal(product);
+// });
+
+// code in the following two lines is added just for testing purposes.
+// // it runs when only iteration 1 is completed. at later point, it can be removed.
+// // end of test
+
+// ITERATION 2
+//... your code goes here
+
+
+// ITERATION 3
+//... your code goes here
+
 
 // ITERATION 4
 
